6d00e326802b6475db520ed2f668674e6fdd67b8
liumengchen@ibantang.com <fbsender@lmc.local>
2017-09-27 12:38:26 +0800

    update app init config

--- app/controllers/mm_controller.rb
+++ app/controllers/mm_controller.rb
@@ -115,6 +115,6 @@ class MmController < ApplicationController
 
   def app_init_config
     config = MmAppInitConfig.take
-    render json: {inreview: config.inreview, update: {updateType: config.update_type, updateUrl: config.update_url, updateText: config.update_text}, appstore_comment_text: config.appstore_comment_text, appstore_comment_button_text: config.appstore_comment_button_text, default_tag: config.default_tag, default_hot: config.default_hot}
+    render json: {inreview: config.inreview, update: {update_type: config.update_type, update_url: config.update_url, update_text: config.update_text, update_version: config.update_version}, appstore_comment_text: config.appstore_comment_text, appstore_comment_button_text: config.appstore_comment_button_text, default_tag: config.default_tag, default_hot: config.default_hot}
   end
 end
