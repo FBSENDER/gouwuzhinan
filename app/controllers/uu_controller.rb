97ea513228c88feeb430ae6464e3ff1c8d459ba1
fbsender <lmctrello@gmail.com>
2018-03-22 14:30:57 +0800

    update

--- app/controllers/uu_controller.rb
+++ app/controllers/uu_controller.rb
@@ -69,7 +69,7 @@ class UuController < ApplicationController
         detail = JSON.parse(lanlan_coupon_detail(item_id)
         token = UuToken.take.token
         url = "https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{token}"
-        Net::HTTP.post_form(URI(url), "touser" => params[:FromUserName], "msgtype" => "link". "link" => {"title" => "#{detail["result"]["couponMoney"].to_i}元优惠券", "description" => "#{detail["result"]["shortTitle"]}", "url" => "http://wap.uuhaodian.com/saber/detail?itemId=#{item_id}&pid=mm_32854514_34792441_314020343&forCms=1&super=1", "thumb_url" => detail["result"]["coverImage"]})
+        Net::HTTP.post_form(URI(url), "touser" => params[:FromUserName], "msgtype" => "link", "link" => {"title" => "#{detail["result"]["couponMoney"].to_i}元优惠券", "description" => detail["result"]["shortTitle"], "url" => "http://wap.uuhaodian.com/saber/detail?itemId=#{item_id}&pid=mm_32854514_34792441_314020343&forCms=1&super=1", "thumb_url" => detail["result"]["coverImage"]})
       end
     rescue
       puts "ERROR: post_message #{item_id}"
