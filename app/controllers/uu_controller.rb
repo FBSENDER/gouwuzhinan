edae53beaef1e8935b5e9da47404b058a7257d60
fbsender <lmctrello@gmail.com>
2018-05-22 23:44:31 +0800

    callback

--- app/controllers/uu_controller.rb
+++ app/controllers/uu_controller.rb
@@ -10,11 +10,7 @@ class UuController < ApplicationController
     page = params[:page].nil? ? 1 : params[:page].to_i
     cid = params[:cid].nil? ? 0 : params[:cid].to_i
     sort = params[:sort].nil? ? 7 : params[:sort].to_i
-    if params[:jsonp]
-      render plain: 'callback(' + lanlan_coupon_list(cid, sort, page, 20) + ')'
-    else
-      render json: lanlan_coupon_list(cid, sort, page, 20)
-    end
+    render json: lanlan_coupon_list(cid, sort, page, 20), callback: params[:callback]
   end
 
   def product
