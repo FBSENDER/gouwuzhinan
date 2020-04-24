44bcfdfbef45a6f362f5f9b2792b2d983f384531
fbsender <lmctrello@gmail.com>
2020-04-25 01:10:17 +0800

    jd api time

--- lib/jdk_api.rb
+++ lib/jdk_api.rb
@@ -5,7 +5,7 @@ def jdk_system_params(method, api_key)
     method: method,
     app_key: api_key,
     access_token: "",
-    timestamp: Time.now.strftime("%F %T"),
+    timestamp: Time.now.localtime.strftime("%F %T"),
     format: "json",
     v: "1.0",
     sign_method: "md5"
